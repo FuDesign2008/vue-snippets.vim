45ee1a9b90216a5e0fd84832f7a43e056ccff2c6
fudesign2008 <fudesign2008@163.com>
2021-07-29 15:57:44 +0800

    new file:   UltiSnips/typescript_vue_tsx.snippets   new file:   src/ComponentName.module.scss   new file:   src/vue.tsx

---
+++ src/vue.tsx
@@ -0,0 +1,43 @@
+/**
+ *
+ *
+ * @author fuyg
+ * @date  2021-07-26
+ */
+
+import {
+    defineComponent,
+    // computed,
+    // toRefs,
+    // ref,
+    // inject,
+    // PropType,
+} from '@vue/composition-api'
+import styles from './ComponentName.module.scss'
+
+export default defineComponent({
+    name: 'ComponentName',
+
+    props: {
+        // TODO
+    },
+
+    setup(/*props*/) {
+        // TODO
+        return {
+            // TODO
+        }
+    },
+
+    render() {
+        // TODO
+        return (
+            <div
+                class={{
+                    'component-name': true,
+                    [styles.componentName]: true,
+                }}
+            ></div>
+        )
+    },
+})
