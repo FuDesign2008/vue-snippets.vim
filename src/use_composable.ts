7bcdbd2a1c6eb932d860b491b7ce892eb6eda1a1
fudesign2008 <fudesign2008@163.com>
2021-09-16 16:48:17 +0800

    new file:   UltiSnips/typescript_use_composable.snippets   new file:   src/use_composable.ts

---
+++ src/use_composable.ts
@@ -0,0 +1,13 @@
+interface UseComposableOption {
+  // TODO
+}
+
+interface UseComposableReturn {
+  // TODO
+}
+
+export function useComposable(
+  option: UseComposableOption,
+): UseComposableReturn {
+  // TODO
+}
