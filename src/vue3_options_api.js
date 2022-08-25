f9b88934f5384e93fd3aaf8d4ec862df46700f0b
fudesign2008 <fudesign2008@163.com>
2022-08-25 10:15:46 +0800

    renamed:    UltiSnips/javascript_vue.snippets -> UltiSnips/javascript_vue2.snippets   renamed:    UltiSnips/javascript_vuex.snippets -> UltiSnips/javascript_vue2_vuex.snippets   new file:   UltiSnips/javascript_vue3_options_api.snippets   renamed:    UltiSnips/typescript_vue.snippets -> UltiSnips/typescript_vue2_class.snippets   renamed:    UltiSnips/typescript_vue_map_vuex.snippets -> UltiSnips/typescript_vue2_class_map_vuex.snippets   renamed:    UltiSnips/typescript_vue_props.snippets -> UltiSnips/typescript_vue2_class_props.snippets   renamed:    UltiSnips/typescript_vue_provide_inject.snippets -> UltiSnips/typescript_vue2_class_provide_inject.snippets   renamed:    UltiSnips/typescript_vuex.snippets -> UltiSnips/typescript_vue2_class_vuex.snippets   renamed:    UltiSnips/typescript_axios.snippets -> UltiSnips/typescript_vue2_class_vuex_axios.snippets   renamed:    UltiSnips/typescript_vue_watch.snippets -> UltiSnips/typescript_vue2_class_watch.snippets   renamed:    UltiSnips/typescript_vue_props_tsx.snippets -> UltiSnips/typescript_vue3_props_tsx.snippets   new file:   UltiSnips/typescript_vue3_tsx.snippets   renamed:    UltiSnips/typescript_use_composable.snippets -> UltiSnips/typescript_vue3_use_composable.snippets   deleted:    UltiSnips/typescript_vue_tsx.snippets   renamed:    src/vuecomp.js -> src/vue2.js   renamed:    src/vuecomp.ts -> src/vue2_class.ts   renamed:    src/vuex.ts -> src/vue2_class_vuex.ts   renamed:    src/vuex_dynamic.ts -> src/vue2_class_vuex_dynamic.ts   renamed:    src/vuex_dynamic_factory.ts -> src/vue2_class_vuex_dynamic_factory.ts   renamed:    src/vuex_from_factory.ts -> src/vue2_class_vuex_dynamic_from_factory.ts   renamed:    src/vuex_dynamic_override_static.ts -> src/vue2_class_vuex_dynamic_override_static.ts   renamed:    src/use_composable.ts -> src/vue2_use_composable.ts   renamed:    src/vuexm.js -> src/vue2_vuex.js   renamed:    src/vuex_map.js -> src/vue2_vuex_map.js   renamed:    src/vue.tsx -> src/vue3.tsx   new file:   src/vue3_options_api.js   renamed:    src/javascript_vue_props.js -> src/vue_props.js

---
+++ src/vue3_options_api.js
@@ -0,0 +1,42 @@
+/**
+ *
+ * @author fuyg
+ * @date  `date +%Y-%m-%d`
+ */
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: '',
+
+  // components: {},
+
+  // props: {},
+
+
+  data() {
+    return {
+      // TODO
+    }
+  },
+
+  // computed: { },
+
+  // watch: { },
+
+  methods: {
+    async fetchData() {
+      // TODO
+    },
+    // TODO
+  },
+
+
+  /**
+   * life cycle
+   */
+  created() {
+    this.fetchData()
+  },
+  // mounted() {},
+  // beforeDestroy() {},
+})
