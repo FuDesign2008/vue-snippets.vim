02a01a4f05e4adb40ce2aa2435191e41cb81cd1f
fudesign2008 <fudesign2008@163.com>
2022-08-25 14:28:37 +0800

    new file:   UltiSnips/javascript_pinia_options_api.snippets   new file:   src/pinia_options_api.js

---
+++ src/pinia_options_api.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author fuyg
+ * @date  `date +%Y-%m-%d`
+ */
+import { defineStore } from 'pinia'
+
+export const useNameStore = defineStore('name', {
+  state: () => {
+    return {
+      // TODO
+    }
+  },
+  actions: {
+    // TODO
+  },
+})
