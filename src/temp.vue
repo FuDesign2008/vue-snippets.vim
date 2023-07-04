0e88ce9effe9cea92188a02e8194128a998b134a
fudesign2008 <fudesign2008@163.com>
2023-07-04 09:37:43 +0800

    new file:   src/temp.vue

---
+++ src/temp.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="component-name">
+    <!-- // TODO -->
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+// import { mapState, mapGetters, mapActions } from 'vuex'
+
+
+export default Vue.extend({
+  name: 'ComponentName',
+
+  // components: {},
+
+  // props: {},
+
+  // mixins: [],
+
+  data() {
+    return {
+      // TODO
+    }
+  },
+
+  computed: {
+    // TODO
+  },
+
+  watch: {
+    // TODO
+  },
+
+  methods: {
+    // TODO
+  },
+
+  /**
+   * life cycle
+   */
+  // beforeCreate() {},
+  // created() { },
+  // mounted() {},
+  // beforeDestroy() {},
+})
+</script>
+<style lang="less" scoped>
+
+</style>
